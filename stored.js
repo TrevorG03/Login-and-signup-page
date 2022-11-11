b7a348d754853ae37a2cc49f6f653262ab87d3a8
MattJMaris <mjmaris221@stevenscollege.edu>
2022-11-11 11:26:50 -0500

    js

--- stored.js
+++ stored.js
@@ -11,3 +11,20 @@ function getUser(username, password) {
 
 var users = [];
 console.log(users)
+
+function myFunction() {
+    var x = document.getElementById("myInput");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
+  function myFunction2() {
+    var x = document.getElementById("myInput2");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
