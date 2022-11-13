db0b911dba40e05160420d2c33dc13b8e2d26bd7
MattJMaris <mjmaris221@stevenscollege.edu>
2022-11-13 13:47:45 -0500

    Attempting to fix password visibility; not fixed

--- stored.js
+++ stored.js
@@ -28,3 +28,86 @@ function myFunction() {
       x.type = "password";
     }
   }
+
+
+
+
+// var inputArray = [];
+// var size = 1; //Maximum Array size
+
+// for(var i=0; i<size; i++) {
+	
+// 	//Taking Input from user
+// 	inputArray[i] = prompt('Enter Username ' + (i+1));
+// }
+
+// //Print the array in the console.
+// console.log(inputArray);
+
+// var inputArray = [];
+// var size = 1; //Maximum Array size
+
+// for(var i=0; i<size; i++) {
+	
+// 	//Taking Input from user
+// 	inputArray[i] = prompt('Enter Password ' + (i+1));
+// }
+
+// //Print the array in the console.
+// console.log(inputArray);
+
+
+
+// var titles = [];
+// var names = [];
+// var tickets = [];
+
+
+// var titleInput = document.getElementById("title");
+// var nameInput = document.getElementById("name");
+// var ticketInput = document.getElementById("tickets");
+
+// var messageBox = document.getElementById("display");
+
+// function insert() {
+//   titles.push(titleInput.value);
+//   names.push(nameInput.value);
+//   tickets.push(ticketInput.value);
+
+//   clearAndShow();
+// }
+
+// function clearAndShow() {
+//   titleInput.value = "";
+//   nameInput.value = "";
+//   ticketInput.value = "";
+
+//   messageBox.innerHTML = "";
+
+//   messageBox.innerHTML += "Titles: " + titles.join(", ") + "<br/>";
+//   messageBox.innerHTML += "Names: " + names.join(", ") + "<br/>";
+//   messageBox.innerHTML += "Tickets: " + tickets.join(", ") + "<br/>";
+// }
+
+
+// function username() {
+//     let userInput = document.querySelector("#userInput");
+//     let message = document.querySelector("#message");
+
+//     message.innerHTML = "Welcome to myMcDonald's, " + userInput.value + "!";
+
+// }
+// function password() {
+//   let passwordInput = document.querySelector("#passwordInput");
+//   let message = document.querySelector("#message");
+
+//   message.innerHTML = "Welcome to myMcDonald's, " + passwordInput.value + "!";
+
+//   var x = document.getElementById("passwordInput");
+//   if (x.type === "password") {
+//     x.type = "text";
+//   } else {
+//     x.type = "password";
+//   }
+// }
+
